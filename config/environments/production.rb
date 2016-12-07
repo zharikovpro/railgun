3f3c11ebb3366e854afadd4174fcd6b673dc21b8
Andrey Zharikov <andrey@zharikov.pro>
2016-12-07 20:53:39 +0300

    cleaner logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,14 +68,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::Logger.new(STDOUT)
   end
 
   # Do not dump schema after migrations.
