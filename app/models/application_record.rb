5ebe9ec4cc4d0188f3292e92c1887cf9154a1f11
Andrey Zharikov <andrey@zharikov.pro>
2016-12-15 01:52:05 +0300

    auto localiza _before_type_cast model attribute names for meaningful format validation errors

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.human_attribute_name(attribute_key_name, options = {})
+    # *_before_type_cast must have same human name for meaningful format validation errors
+    super(attribute_key_name.to_s.remove!('_before_type_cast'), options)
+  end
 end
