5d1a398063a20071a5879abba33c216faa78fe30
Andrey Zharikov <andrey@zharikov.pro>
2016-12-15 14:54:07 +0300

    auto translate reference column name in forms and inside admin area

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.human_attribute_name(attribute_key_name, options = {})
+    # *_id must have same human name for localized forms with references and admin area lists
     # *_before_type_cast must have same human name for meaningful format validation errors
-    super(attribute_key_name.to_s.remove!('_before_type_cast'), options)
+    super(attribute_key_name.to_s.remove('_id', '_before_type_cast'), options)
   end
 end
