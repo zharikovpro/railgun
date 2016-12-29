f7ff0b9e53800f3b0f9a659984e9d001b6c835d7
Andrey Zharikov <andrey@zharikov.pro>
2016-12-30 00:26:42 +0300

    active admin tweaks extracted to the Instant Data Manager

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,5 +1,3 @@
-# See Railgun tweaks below config section
-
 ActiveAdmin.setup do |config|
   # == Site Title
   #
@@ -272,18 +270,3 @@ ActiveAdmin.setup do |config|
   #
   # config.include_default_association_filters = true
 end
-
-# Railgun tweaks
-
-module DefaultMainContentWithTimestampsInTheEnd
-  def default_attribute_table_rows
-    columns = super
-
-    [:deleted_at, :updated_at, :created_at].each do |column|
-      columns << column if columns.delete(column)
-    end
-
-    columns
-  end
-end
-ActiveAdmin::Views::Pages::Show.prepend(DefaultMainContentWithTimestampsInTheEnd)
