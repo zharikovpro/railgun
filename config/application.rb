9aeddd9b8eb4020b2875b3ad19b2f6d995b49acc
Andrey Zharikov <andrey@zharikov.pro>
2016-12-30 18:52:49 +0300

    db schema in sql format

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module Railgun
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Benefits of explicit DDL in migrations and structure:
+    # + don't fight with migration generator that forgets about important stuff like null: false, indexes, checks, etc
+    # + can generate DDL using DBA tools like DataGrip / DbSchema / whatever
+    # + support advanced PostgreSQL features like deferred constraints, etc
+    # + dump complete structure in SQL format for better integration with SQL utilities
+    config.active_record.schema_format = :sql
   end
 end
