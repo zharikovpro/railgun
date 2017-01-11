c2f90bc760d8f75bd0985f4751fe3ef7eb218c52
Andrey Zharikov <andrey@zharikov.pro>
2017-01-12 01:46:05 +0300

    missing routes require

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  require 'sidekiq/web'
   authenticate :admin_user do
     mount Sidekiq::Web => "/#{ENV.fetch('ADMIN_NAMESPACE', :cowboy)}/sidekiq"
   end
