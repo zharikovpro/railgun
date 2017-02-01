78ae69a276de5d1d04d45a430d210d7c4bed2388
zharikovpro <andrey@zharikov.pro>
2017-02-01 23:44:26 +0300

    wip: reincarnation

--- app/controllers/admin/reincarnation_controller.rb
+++ app/controllers/admin/reincarnation_controller.rb
@@ -1,14 +1,18 @@
 class ReincarnationController < ApplicationController
   def create
+    authorize :reincarnation
+
     session[:reincarnated_employee_id] = current_employee.id
     user = User.find(params[:user_id])
     sign_in user
+
     redirect_to root_path, notice: "Reincarnated as #{user.email}"
   end
 
   def destroy
     sign_in Employee.find(session[:reincarnated_employee_id])
     session.delete(:reincarnated_employee_id)
+
     redirect_to admin_path, notice: 'Returned from reincarnation'
   end
 end
