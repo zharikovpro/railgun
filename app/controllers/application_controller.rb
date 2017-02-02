f4c4ffa3ef30f4cbc135560d7de2ba8af1b15270
zharikovpro <andrey@zharikov.pro>
2017-02-02 15:01:55 +0300

    build fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :active_admin_root_path, reincarnation?
+  helper_method :active_admin_root_path, :reincarnation?
 
   protect_from_forgery with: :exception
 
