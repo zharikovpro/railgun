155536cac726748c938c90c3d23db75e580a995d
Andrey Zharikov <andrey@zharikov.pro>
2017-03-31 21:47:27 +0300

    Green rake ci

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -1,9 +1,9 @@
-RSpec.describe do
+RSpec.describe UserRole, issues: [54] do
   it 'belongs to user' do
     it.is_expected.to belong_to(:user)
   end
 
-  it 'has predefined list of roles' do
+  it 'has predefined list of possible roles' do
     it.is_expected.to define_enum_for(:status).with([:administrator, :developer, :support, :moderator])
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe do
-  it 'may have many roles' do
+  it 'may have many roles', issues: [54] do
     it.is_expected.to have_many(:roles)
   end
 end
