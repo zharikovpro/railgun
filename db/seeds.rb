934022cb1c3378dcb8b135b4aa4f07a34fdc1f3a
Andrey Zharikov <andrey@zharikov.pro>
2017-04-08 20:36:59 +0300

    Cleaner db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-administrator = User.new(email: 'admin', password: 'admin', password_confirmation: 'admin')
-administrator.save
+FactoryGirl.create(:administrator, email: 'admin', password: 'admin', password_confirmation: 'admin')
