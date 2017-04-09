efa3d378db43c1cb5a8d19df0ac39477fddf560b
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-04-09 18:49:10 +0500

    policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,4 +1,19 @@
 class UserPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user  = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.administrator?
+        scope.all
+      end
+    end
+  end
+
   def create?
     user.administrator?
   end
