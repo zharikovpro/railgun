1b0dbc94d10b76abfddac8216de7e2a5ffdd0a7e
Andrey Zharikov <andrey@zharikov.pro>
2017-04-12 00:05:33 +0300

    todo cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception # TODO: unless current_user.visitor?
+  protect_from_forgery with: :exception
 
   before_action :set_paper_trail_whodunnit
 
