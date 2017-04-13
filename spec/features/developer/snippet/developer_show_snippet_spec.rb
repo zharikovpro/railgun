9628d3a66e09bd176ed3119695090e28ae45c9f8
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-04-13 19:08:03 +0500

    use js alert

--- spec/features/developer/snippet/developer_show_snippet_spec.rb
+++ spec/features/developer/snippet/developer_show_snippet_spec.rb
@@ -6,16 +6,17 @@ HEREDOC
 
 RSpec.feature feature, issues: [41] do
   scenario = <<~HEREDOC
-    Given snippet with slug 'head' and text '<script>document.write('test');</script>'
+    Given snippet with slug 'head' and text '<script>alert('test');</script>'
     When developer visits root page
     Then he sees 'test'
   HEREDOC
 
-  scenario scenario do
-    create(:snippet, slug: 'head', text: "<script>document.write('test');</script>")
+  scenario scenario, :js do
+    create(:snippet, slug: 'head', text: "<script>alert('test');</script>")
+    login_as create(:developer)
 
     visit root_path
 
-    expect(page).to have_content('test')
+    expect(page.accept_alert).to eq('test')
   end
 end
