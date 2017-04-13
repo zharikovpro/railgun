f6f0e556c855fc93db9f347e46e1c5134ba6ef45
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-04-13 13:40:02 +0500

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#root'
 
-  resources :snippets, only: :index
-
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
 
