998e36a7ae00b58a229d369c51ea89ce29fee465
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-04-14 11:29:12 +0500

    add config

--- spec/views/layouts/application.html.slim_spec.rb
+++ spec/views/layouts/application.html.slim_spec.rb
@@ -1,3 +1,9 @@
 RSpec.describe 'layouts/application' do
-  it 'given snippet with slug "head" renders it right before </head>', issues: [41]
+  it 'given snippet with slug "head" renders it right before </head>', issues: [41] do
+    snippet = create(:snippet, slug: 'head')
+
+    render template: 'application/root', layout: 'layouts/application'
+
+    expect(response).to match /#{snippet.text}<\/head>/
+  end
 end
