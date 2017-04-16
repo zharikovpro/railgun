b767cb02264dd0f90bbee0934e1236522c2747d6
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-04-16 16:36:51 +0500

    busy roles

--- app/admin/user_role.rb
+++ app/admin/user_role.rb
@@ -7,13 +7,7 @@ ActiveAdmin.register UserRole do
     f.semantic_errors(*f.object.errors.keys)
     f.inputs 'Roles' do
       f.div User.find_by_id(params[:user_id]).try(:email)
-=begin
-      UserRole::TITLES.each do |role|
-        if User.find_by_id(params[:user_id]).roles.include? role
-        end
-      end
-=end
-      f.input :role, as: :select, collection: UserRole::TITLES
+      f.input :role, as: :select, collection: UserRole::TITLES - User.find_by_id(params[:user_id]).roles
       f.input :user_id, input_html: {value: params[:user_id]}, as: :hidden
     end
 
