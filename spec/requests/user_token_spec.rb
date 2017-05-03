ac9a55dbf1b6ccde42b65b98f7b7b8345329b70f
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-03 14:23:59 +0500

    api done

--- spec/requests/user_token_spec.rb
+++ spec/requests/user_token_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UserTokenController, issues: [116] do
-  describe 'authenticate' do
-    it 'authenticates correctly' do
-      user = create(:user)
-      page_id = create(:page).id
+  it 'authenticates correctly' do
+    user = create(:user)
+    page_id = create(:page).id
 
-      post '/api/v1/user_token', auth: { email: user.email, password: user.password }, format: 'json'
-      token = response.parsed_body['jwt']
-      get "/api/v1/pages/#{page_id}", headers: { 'Authorization' => "Bearer #{token}" }
+    post '/api/v1/user_token', auth: { email: user.email, password: user.password }, format: 'json'
+    token = response.parsed_body['jwt']
+    get "/api/v1/pages/#{page_id}", headers: { 'Authorization' => "Bearer #{token}" }
 
-      expect(response.parsed_body['id']).to eq(page_id)
-      expect(response).to have_http_status(200)
-    end
+    expect(response.parsed_body['id']).to eq(page_id)
+    expect(response).to have_http_status(200)
   end
 end
 
 RSpec.describe 'the requests support CORS headers', issues: [113] do
-  fit 'Returns the response CORS headers' do
-    get '/api/v1/pages', nil, 'HTTP_ORIGIN' => 'http://super-test.com', 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' => 'GET'
+  it 'Returns the response CORS headers' do
+    get '/api/v1/pages', nil, 'HTTP_ORIGIN' => 'http://super-test.com'
 
-    expect(response.headers['Access-Control-Allow-Origin']).to eq('http://super-test.com')
     expect(response.headers['Access-Control-Allow-Origin']).to eq('http://super-test.com')
     expect(response.headers['Access-Control-Allow-Methods']).to eq('OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE')
     expect(response.headers['Access-Control-Allow-Headers']).to be_nil
