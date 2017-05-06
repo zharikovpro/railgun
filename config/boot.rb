dcde04f88e0c1d74880756e57cde858f8f4c2071
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-06 12:35:53 +0500

    update rails to 5.1

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+=begin
 require 'rails/commands/server'
 
 module DefaultOptions
@@ -10,3 +11,4 @@ module DefaultOptions
 end
 
 Rails::Server.prepend(DefaultOptions)
+=end
