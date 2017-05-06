c185bde1555e5322abf3004477a55ed2f6b2df41
Andrey Zharikov <andrey@zharikov.pro>
2017-05-06 22:52:25 +0300

    removed old hack to run Rails on the port 5000 as default, no need for that

--- config/boot.rb
+++ config/boot.rb
@@ -1,12 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'rails/commands/server'
-
-module DefaultOptions
-  def default_options
-    super.merge!(Port: 5000) # default for `heroku local`
-  end
-end
-
-Rails::Server.prepend(DefaultOptions)
