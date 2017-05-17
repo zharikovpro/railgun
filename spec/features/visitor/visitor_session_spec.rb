35c50f519f3e55fa6c02be5e4c7c2f82587ed1bd
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-17 11:46:22 +0500

    codeclimate#2

--- spec/features/visitor/visitor_session_spec.rb
+++ spec/features/visitor/visitor_session_spec.rb
@@ -1,14 +1,14 @@
 feature = <<~HEREDOC
   When visitor login he gets session,
   he have not been active in a specified period of time
-  so expires session and employee is logout
+  so expires session and visitor is logout
 HEREDOC
 
 RSpec.feature feature, issues: [111] do
   scenario = <<~HEREDOC
-    Given user is logined
+    Given visitor is logined
     When he have not been active in a 7 days
-    Then expires session and user is logout
+    Then expires session and visitor is logout
   HEREDOC
 
   scenario scenario do
