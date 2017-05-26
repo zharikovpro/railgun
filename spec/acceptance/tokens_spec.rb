bd687485fbc808254bd6cddb9e933ee5741688e9
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-26 17:52:31 +0500

    just one test

--- spec/acceptance/tokens_spec.rb
+++ spec/acceptance/tokens_spec.rb
@@ -2,7 +2,7 @@ RSpec.resource 'Tokens', issues: ['railgun#149'] do
   header 'Host', 'localhost:5000'
   header 'Content-Type', 'application/json'
 
-  let(:user) { create(:user) }
+  let(:user) { create(:editor) }
 
   post '/api/v1/tokens' do
     parameter :auth, 'Token'
@@ -15,20 +15,9 @@ RSpec.resource 'Tokens', issues: ['railgun#149'] do
       explanation 'Create the new snippet'
 
       expect(status).to eq 201
+      @@token = JSON.parse(response_body)['jwt']
+      puts @@token
+      puts user.api_token
     end
   end
 end
-=begin
-it 'authenticates correctly' do
-  user = create(:user)
-  page_id = create(:page).id
-
-  post '/api/v1/tokens', params: { auth: { email: user.email, password: user.password } }
-  token = response.parsed_body['jwt']
-  expect(response).to have_http_status(201)
-  get "/api/v1/pages/#{page_id}", headers: { 'Authorization' => "Bearer #{token}" }
-
-  expect(response.parsed_body['id']).to eq(page_id)
-  expect(response).to have_http_status(200)
-end
-=end
