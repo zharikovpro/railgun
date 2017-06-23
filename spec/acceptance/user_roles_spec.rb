78c2e4c277c9379d106dbf4240eea2d70606ad21
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-06-23 17:17:22 +0500

    post complete

--- spec/acceptance/user_roles_spec.rb
+++ spec/acceptance/user_roles_spec.rb
@@ -3,7 +3,7 @@ RSpec.resource 'UserRoles', issues: ['railgun#168'] do
   header 'Content-Type', 'application/json'
   before { header 'Authorization', "Bearer #{create(:administrator).api_token}" }
   let!(:user) { create(:user) }
-  let!(:role) { user.add_role(:owner) }
+  let!(:user_role) { user.add_role(:owner) }
   response_field :id, 'ID', 'Type' => 'Integer'
   response_field :user_id, 'user ID', 'Type' => 'Integer'
   response_field :roles, 'Name of role', 'Type' => 'String'
@@ -26,7 +26,7 @@ RSpec.resource 'UserRoles', issues: ['railgun#168'] do
   end
 
   get '/api/v1/user_roles/:id' do
-    let(:id) { role.id }
+    let(:id) { user_role.id }
     let(:roles) { user.roles }
 
     example_request 'Get specific user_role' do
@@ -39,10 +39,10 @@ RSpec.resource 'UserRoles', issues: ['railgun#168'] do
 
   post '/api/v1/user_roles' do
     parameter :user_id, 'User ID'
-    parameter :user_role, 'User role'
+    parameter :role, 'User role'
 
-    let(:user_id) { user.id }
-    let(:user_role) { 'editor' }
+    let(:user_id) { "#{user.id}" }
+    let(:role) { 'editor' }
 
     let(:raw_post) { params.to_json }
 
@@ -50,8 +50,8 @@ RSpec.resource 'UserRoles', issues: ['railgun#168'] do
       explanation 'Create the new user role'
 
       expect(status).to eq 201
-      expect(JSON.parse(response_body)['user_role']).to eq('editor')
-      expect(User.find(user.id).roles).to match('editor')
+      expect(JSON.parse(response_body)['role']).to include('editor')
+      expect(User.find(user.id).roles).to include(:editor)
     end
   end
 end
