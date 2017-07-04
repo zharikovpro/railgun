f8fe0ef127400f88ab4a04d9f8ffd994c189792d
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-04 15:12:20 +0500

    add features delete media

---
+++ spec/features/employee/editor/media/editor_delete_media_spec.rb
@@ -0,0 +1,25 @@
+feature = <<~HEREDOC
+  When editor wants to delete media,
+  he wants to delete specific media with 'img' slug,
+  so that media file will be removed
+HEREDOC
+
+RSpec.feature feature, issues: ['railgun#184'] do
+  scenario = <<~HEREDOC
+    Given editor is on the Medias page
+    When he clicks 'Delete' and accept confirm
+    Then media file record has removed
+  HEREDOC
+
+  scenario scenario, :js do
+    media = create(:media, slug: 'img')
+    login_as create(:editor)
+    visit staff_medias_path
+
+    click_link 'Delete', href: staff_media_path(media)
+    page.accept_alert
+
+    expect(page).not_to have_content(media.slug)
+    expect(Media.find_by_slug('img')).to be_nil
+  end
+end
