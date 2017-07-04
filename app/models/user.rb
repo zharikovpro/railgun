ac8092388f58585fcd51303fffa9e91dc9d3d815
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-04 08:08:11 +0500

    remove \z

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :timeoutable
 
   # https://davidcel.is/posts/stop-validating-email-addresses-with-regex/
-  validates_format_of :email, with: /.*@.*\z/
+  validates_format_of :email, with: /.*@.*/
 
   validates_presence_of :password, if: :password_required?
   validate :password_match?, if: :password_required?
