7e74219d648b98c3c6713459df2b0fa6ea9b5161
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-06 16:11:16 +0500

    fixes # 186

--- spec/features/employee/administrator/user/administrator_delete_user_spec.rb
+++ spec/features/employee/administrator/user/administrator_delete_user_spec.rb
@@ -5,6 +5,10 @@ feature = <<~HEREDOC
 HEREDOC
 
 RSpec.feature feature, issues: ['railgun#178'] do
+  before { login_as create(:administrator) }
+  let!(:user) { create(:user) }
+  let!(:employee) { create(:owner) }
+  before { visit staff_users_path }
 
   scenario = <<~HEREDOC
     Given administrator is on the Users page
@@ -13,15 +17,24 @@ RSpec.feature feature, issues: ['railgun#178'] do
   HEREDOC
 
   scenario scenario, :js do
-    user = create(:user)
-
-    login_as create(:administrator)
-    visit staff_users_path
-
     click_link 'Delete', href: staff_user_path(user)
     page.accept_alert
 
     expect(page).not_to have_content(user.email)
     expect(User.find_by_id(user.id)).to be_nil
+  end
+
+  scenario = <<~HEREDOC
+    Given administrator is on the Users page
+    When he clicks 'Delete' and accepts confirmation
+    Then employee record has removed with all roles
+  HEREDOC
+
+  scenario scenario, :js do
+    click_link 'Delete', href: staff_user_path(employee)
+    page.accept_alert
+
+    expect(page).not_to have_content(employee.email)
+    expect(User.find_by_id(employee.id)).to be_nil
   end
 end
