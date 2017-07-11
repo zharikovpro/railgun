0901d73ca780d69c3ca244f93a360a3ebeaf5647
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-11 11:16:26 +0500

    remove password_validation, because it already exists in device

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ApplicationRecord
   # consider using :lockable when user has financial transactions
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :timeoutable, :validatable
 
-  validates_presence_of :password, if: :password_required?
-  validate :password_match?, if: :password_required?
-
   has_many :user_roles, dependent: :destroy
   accepts_nested_attributes_for :user_roles, allow_destroy: true
   alias_method :authenticate, :valid_password?
@@ -44,22 +41,6 @@ class User < ApplicationRecord
 
   def editor?
     roles.include?(:editor)
-  end
-
-  def password_required?
-    # Password is required if it is being set, but not for new records
-    if persisted?
-      !password.nil? || !password_confirmation.nil?
-    else
-      true
-    end
-  end
-
-  def password_match?
-    self.errors[:password] << 'cannot be blank' if password.blank?
-    self.errors[:password_confirmation] << 'cannot be blank' if password_confirmation.blank?
-    self.errors[:password_confirmation] << 'does not match' if password != password_confirmation
-    password.present? && password == password_confirmation
   end
 
   def api_token
