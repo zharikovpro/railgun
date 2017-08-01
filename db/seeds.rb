161f563cdf7a6544a94a93bfa53f2e8370119247
mifrill <alexei.mifrill.strizhak@gmail.com>
2017-08-01 01:07:39 -0700

    change password for db:seeds corrected
    add travis CI server configuration file
    
    add database.yml.travis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-administrator = FactoryGirl.create(:user, email: 'root@mail.com', password: 'root', password_confirmation: 'root')
+administrator = FactoryGirl.create(:user, email: 'root@mail.com', password: 'password', password_confirmation: 'password')
 UserRole::TITLES.each { |title| UserRole.create(user: administrator, role: title) }
